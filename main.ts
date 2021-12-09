2f65ca75ca81b29e991ff7036c51afbc090b2533
CodeUtkan <78762546+CodeUtkan@users.noreply.github.com>
2021-12-09 22:48:30 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+namespace RcCar
+{
+    //% blockId=rcCar
+    //% block="RCar SagMotor=$hiz1, SolMotor=$hiz"
+    //% hiz.defl=value hiz.defl=value
+    //% hiz.min=0 hiz.max=100
+    //% hiz1.min=0 hiz.max=100
+    //% color="#8B0000"
+
+
 radio.onReceivedValue(function (name, value) {
     if (name == "sag") {
         sagMotor(value)
@@ -7,10 +17,10 @@ radio.onReceivedValue(function (name, value) {
         dur()
     }
 })
-function sagMotor (hiz: number) {
+export function sagMotor(hiz1: number): void {
     pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.analogWritePin(AnalogPin.P11, Math.abs(hiz))
-    if (hiz > 0) {
+    pins.analogWritePin(AnalogPin.P11, Math.abs(hiz1))
+    if (hiz1 > 0) {
         pins.digitalWritePin(DigitalPin.P12, 0)
         pins.digitalWritePin(DigitalPin.P2, 1)
     } else {
@@ -18,7 +28,7 @@ function sagMotor (hiz: number) {
         pins.digitalWritePin(DigitalPin.P2, 0)
     }
 }
-function solMotor (hiz: number) {
+export function solMotor(hiz: number): void {
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.analogWritePin(AnalogPin.P15, Math.abs(hiz))
     if (hiz > 0) {
@@ -29,12 +39,13 @@ function solMotor (hiz: number) {
         pins.digitalWritePin(DigitalPin.P14, 0)
     }
 }
-function dur () {
+export function dur(): void {
     pins.digitalWritePin(DigitalPin.P12, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
 }
-radio.setGroup(2)
+radio.setGroup(5)
 led.enable(false)
+}
